784759d697ce435603dd1a40152e483d8006ece8
luferapaza@gmail.com <35202712+MrLufer@users.noreply.github.com>
2018-02-21 09:35:06 -0500

    ..

--- models/user.js
+++ models/user.js
@@ -2,41 +2,27 @@ const bcrypt = require('bcrypt-nodejs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
   ruc: {type: Number ,unique: true, required:true},
-  password: {type: String, required:true},
+  password: {type: String, required:true, select:false},
   name: {type: String, required:true}
-},{
-  timestamps:true
-})
-
-userSchema.pre('save',function(next){
-  const user = this;
-  if(!user.isModified('password')){
-    return next();
-  }
-  bcrypt.genSalt(10,(err,salt)=>{
-    if (err){
-      next(err);
-    }
-    bcrypt.hash(user.password,salt ,null,(err,hash)=>{
-      if(err){
-        next(err);
-      }
-      user.password = hash;
-      next();
-    });
-  })
-})
-
-
-userSchema.methods.compararpasword = function(password,cb){
-  bcrypt.compare(password, this.password,(err,sonIguales)=>{
-    if(err){
-      return cb(err);
-    }
-    cb(null,sonIguales);
-  })
+  singupDate {type: Date, default: Date.now()},
+  lastLogin Date
 }
 
-module.exports = mongoose.model('User',userSchema);
+UserSchema.pre('save',(next)=>{
+  let user = this
+  if(!user.isModified('password')) return next()
+
+  bcrypt.genSalt(10,(err,salt)=>{
+    if (err) return next(err)
+    bcrypt.hash(user.password,salt,null,(err,hash)=>{
+      if(err) return next(err)
+
+      user.password = hash
+      next()
+    })
+  })
+})
+
+module.exports= mongoose.model('User',UserSchema)
